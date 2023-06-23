ee3488328cb16232815b9c2304f57b4b15bd88ca
anastasia_kharchenko <anastasia.kharchenko@sytoss.com>
2023-06-24 01:28:43 +0300

    Added link to the product

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.tsx'
 import './index.css'
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route index={true} element={<HomePage />}></Route>
+            <Route path="product/:slug" element={<ProductPage />} />
+        </Route>
+    )
+);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
