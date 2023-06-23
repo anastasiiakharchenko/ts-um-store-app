5fae71d34a92de65d82e0b8bf5c08e9636d5d65b
anastasia_kharchenko <anastasia.kharchenko@sytoss.com>
2023-06-24 00:49:17 +0300

    Added product list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
+import {sampleProducts} from "./data";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <div>
-      <header>Um Store</header>
-      <main></main>
-      <footer>All right reserved</footer>
-    </div>
-  )
+    return (
+        <div>
+            <header>Um Store</header>
+            <main>
+                <ul>
+                    {
+                        sampleProducts.map(product => (
+                            <li key={product.slug}>
+                                <img
+                                    src={product.image}
+                                    alt={product.name}
+                                    className="product-image"
+                                />
+                                <h2>{product.name}</h2>
+                                <p>{product.price}</p>
+                            </li>))
+                    }
+                </ul>
+            </main>
+            <footer>All right reserved</footer>
+        </div>
+    )
 }
 
 export default App
